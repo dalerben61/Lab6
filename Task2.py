f1c8c9a509aa4ee1fb71011dafb93fd68754142f
unknown <ben.delisle61@gmail.com>
2019-10-14 02:18:34 -0400

    Added the Task 2

---
+++ Task2.py
@@ -0,0 +1,48 @@
+from gfxhat import lcd
+from PIL import Image, ImageFont, ImageDraw
+
+lcd.clear()
+lcd.show()
+
+f1 =  [
+[1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1],
+[0,1,1,1,1,1,1,0],
+[1,0,1,1,1,1,0,1],
+[1,0,0,1,1,0,0,1],
+[1,0,0,1,1,0,0,1],
+[0,0,0,1,1,0,0,0],
+[0,0,0,0,0,0,0,0]
+]
+
+pm = [[0,0,0,1,1,1,1,1,0,0,0],
+[0,0,1,1,1,1,1,1,1,0,0],
+[0,1,1,1,1,1,1,1,1,1,0],
+[1,1,1,1,1,1,1,1,0,0,0],
+[1,1,1,1,1,1,1,0,0,0,0],
+[1,1,1,1,1,1,0,0,0,0,0],
+[1,1,1,1,1,1,0,0,0,0,0],
+[1,1,1,1,1,1,1,0,0,0,0],
+[1,1,1,1,1,1,1,1,0,0,0],
+[0,1,1,1,1,1,1,1,1,1,0],
+[0,0,1,1,1,1,1,1,1,0,0],
+[0,0,0,1,1,1,1,1,0,0,0]
+]
+
+def displayObject(obj,x,y):
+    lcd.set_pixel(x, y, 1)
+    lcd.show() #The function is where I don't know what I need to do
+
+choice = int(input("Which of the two objects do you want to display? (1 or 2)"))
+
+if choice == 1:
+    obj = f1
+elif choice == 2:
+    obj = pm
+
+x = int(input("Choose a starting value for x:"))
+y = int(input("Choose a starting value for y:"))
+
+displayObject(obj, x, y)
+        
+
