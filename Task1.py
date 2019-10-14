a78c32b3824acd1a85a5aefa392f8151546f373e
unknown <ben.delisle61@gmail.com>
2019-10-13 22:31:59 -0400

    Finished to program the Etch a Sketch

--- Task1.py
+++ Task1.py
@@ -1,8 +1,23 @@
 from gfxhat import lcd, fonts, backlight
 from PIL import Image, ImageFont, ImageDraw
+import click
 
-backlight.set_all(255,0,255)
+
+backlight.set_all(255,0,130)
 backlight.show()
+lcd.clear()
+lcd.show()
+x = 0
+y = 0
+
+
+print("This program creates a simulation of an Etch a Sketch on the GFX Hat.")
+print("Here are a few instruction on how to use the program.")
+print("Use the arrow keys to draw lines on the GFX Hat.")
+print("Press S to clear the screen.")
+print("Press Q to exit.")
+print("Press S now to erase the 'Etch a Sketch' message diplayed on the hat")
+
 
 def displayText():
     lcd.clear()
@@ -21,5 +36,47 @@ def displayText():
             lcd.set_pixel(x, y, pixel)
     lcd.show() 
 
-print("Does it work?")
 displayText()
+
+while True:
+    input = click.getchar()
+    click.echo()
+
+    if input == 's':
+        lcd.clear()
+        lcd.set_pixel(x,y,1)
+        lcd.show()
+    
+    if input == 'q':
+        break
+    
+    if input == '\x1b[A':
+        y = y - 1
+        if y < 0:
+            y = 63
+        lcd.set_pixel(x,y,1)
+        lcd.show()
+    elif input == '\x1b[B':
+        y = y + 1
+        if y > 63:
+            y = 0
+        lcd.set_pixel(x,y,1)
+        lcd.show()
+    elif input == '\x1b[C':
+        x = x + 1
+        if x > 127:
+            x = 0
+        lcd.set_pixel(x,y,1)
+        lcd.show()
+    elif input == '\x1b[D':
+        x = x - 1
+        if x < 0:
+            x = 127
+        lcd.set_pixel(x,y,1)
+        lcd.show()
+    
+print("Goodbye!")
+backlight.set_all(0,0,0)
+backlight.show()
+lcd.clear()
+lcd.show()
