45d60326604c5fe5452789408ea7d13b0cc799f2
unknown <ben.delisle61@gmail.com>
2019-10-26 14:25:29 -0400

    Corrected Task2 to work the intended way

--- Task2.py
+++ Task2.py
@@ -1,5 +1,4 @@
 from gfxhat import lcd
-from PIL import Image, ImageFont, ImageDraw
 
 lcd.clear()
 lcd.show()
@@ -15,7 +14,8 @@ f1 =  [
 [0,0,0,0,0,0,0,0]
 ]
 
-pm = [[0,0,0,1,1,1,1,1,0,0,0],
+pm = [
+[0,0,0,1,1,1,1,1,0,0,0],
 [0,0,1,1,1,1,1,1,1,0,0],
 [0,1,1,1,1,1,1,1,1,1,0],
 [1,1,1,1,1,1,1,1,0,0,0],
@@ -30,19 +30,30 @@ pm = [[0,0,0,1,1,1,1,1,0,0,0],
 ]
 
 def displayObject(obj,x,y):
-    lcd.set_pixel(x, y, 1)
-    lcd.show() #The function is where I don't know what I need to do
+    lcd.clear()
+    initialPositionx = x
+    initialPositiony = y
+    for i in obj:
+        for j in i:
+            if j == 1:
+                lcd.set_pixel(initialPositionx, initialPositiony, 1)
+                lcd.show()
+            initialPositionx += 1
+            if x > 127:
+                x = 0
+        initialPositiony+=1
+        if y > 63:
+            y = 0
+        initialPositionx = x
+            
+
 
 choice = int(input("Which of the two objects do you want to display? (1 or 2)"))
-
-if choice == 1:
-    obj = f1
-elif choice == 2:
-    obj = pm
-
 x = int(input("Choose a starting value for x:"))
 y = int(input("Choose a starting value for y:"))
 
-displayObject(obj, x, y)
-        
+if choice == 1:
+    displayObject(f1,x,y)
+elif choice == 2:
+    displayObject(pm,x,y)
 
